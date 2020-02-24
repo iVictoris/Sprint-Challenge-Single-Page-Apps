06fd94405e01a1fe580fe85ab79234b3b0d1018f
Victor Tran <victor.n.tran@outlook.com>
2020-02-23 21:15:29 -0500

    add state for characters, sent request for characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
+  //   - Fetch a list of characters from the Rick and Morty API's Characters endpoint *`https://rickandmortyapi.com/api/character/`* and render them to the screen.
+  //   - You must display at least one element for each character.
+
   // TODO: Add useState to track data from useEffect
+
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+
+    const url = `https://rickandmortyapi.com/api/character/`;
+    const fetchCharacters = async () => {
+      // response > data > results
+      const {
+        data: { results }
+      } = await axios.get(url);
+      setCharacters(results);
+    };
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    fetchCharacters().catch(err => console.log(err));
   }, []);
+  console.log(characters);
 
   return (
     <section className="character-list">
