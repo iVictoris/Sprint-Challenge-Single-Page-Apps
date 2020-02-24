05fd2662c422dd4414b2d6c3e25abc7389a6407a
Victor Tran <victor.n.tran@outlook.com>
2020-02-23 21:33:39 -0500

    Hook up router to link to welcome page and characters page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Header from "./components/Header";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage";
+
+import { Switch, Route, NavLink, Redirect } from "react-router-dom";
 
 export default function App() {
   return (
-    <main>
-      <Header />
-      <CharacterList />
-    </main>
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="characters/">Characters</NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/characters/" component={CharacterList} />
+          <Route
+            path="/"
+            exact
+            render={() => {
+              return <WelcomePage />;
+            }}
+          />
+
+          <Redirect from="*" to="/" />
+        </Switch>
+      </main>
+    </>
   );
 }
