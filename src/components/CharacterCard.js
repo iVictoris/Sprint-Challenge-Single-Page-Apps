efbb170eea07284ffafd91be61c2c6386ee3202d
Victor Tran <victor.n.tran@outlook.com>
2020-02-23 21:23:11 -0500

    take out character data as needed and display minimum for now

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ charData }) {
+  /* id name, species, gender, image
+   *
+   */
+  const { id, name, species, gender, image } = charData;
+
+  return (
+    <div className="CharacterCard">
+      <header>
+        <h3>{name}</h3>
+      </header>
+
+      <div>
+        <img src={image} />
+      </div>
+    </div>
+  );
 }
